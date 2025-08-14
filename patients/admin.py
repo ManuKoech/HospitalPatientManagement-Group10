65138be7e60714f467f1b579d08f57e9c7637896
egkimari <erick.githinji@strathmore.edu>
2025-08-14 14:59:23 +0300

    view + urls.py

--- patients/admin.py
+++ patients/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Department, Doctor, Patient, Appointment, MedicalRecord, Billing
 
-# Register your models here.
+admin.site.register(Department)
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Appointment)
+admin.site.register(MedicalRecord)
+admin.site.register(Billing)
--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Department, Doctor, Patient, Appointment, MedicalRecord, Billing
+from .serializers import (
+    DepartmentSerializer, DoctorSerializer, PatientSerializer,
+    AppointmentSerializer, MedicalRecordSerializer, BillingSerializer
+)
 
-# Create your views here.
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+class DoctorViewSet(viewsets.ModelViewSet):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
+
+class PatientViewSet(viewsets.ModelViewSet):
+    queryset = Patient.objects.all()
+    serializer_class = PatientSerializer
+
+class AppointmentViewSet(viewsets.ModelViewSet):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+
+class MedicalRecordViewSet(viewsets.ModelViewSet):
+    queryset = MedicalRecord.objects.all()
+    serializer_class = MedicalRecordSerializer
+
+class BillingViewSet(viewsets.ModelViewSet):
+    queryset = Billing.objects.all()
+    serializer_class = BillingSerializer
