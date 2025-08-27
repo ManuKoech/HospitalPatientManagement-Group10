f7b9e72b78e0d11007b2504912da0500e6f781c2
aymaan-coutinho <130558460+aymaan-coutinho@users.noreply.github.com>
2025-08-27 20:50:40 +0300

    Update views.py

--- patients/views.py
+++ patients/views.py
@@ -5,26 +5,33 @@ from .serializers import (
     AppointmentSerializer, MedicalRecordSerializer, BillingSerializer
 )
 
+
 class DepartmentViewSet(viewsets.ModelViewSet):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
+    
 
 class DoctorViewSet(viewsets.ModelViewSet):
     queryset = Doctor.objects.all()
     serializer_class = DoctorSerializer
 
+
 class PatientViewSet(viewsets.ModelViewSet):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+
 
 class AppointmentViewSet(viewsets.ModelViewSet):
     queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
 
+
 class MedicalRecordViewSet(viewsets.ModelViewSet):
     queryset = MedicalRecord.objects.all()
     serializer_class = MedicalRecordSerializer
 
+
 class BillingViewSet(viewsets.ModelViewSet):
     queryset = Billing.objects.all()
     serializer_class = BillingSerializer
+
